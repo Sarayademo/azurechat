0094056fb9a23026a10928bb6808c915b07b5208
Sarayademo <153470181+Sarayademo@users.noreply.github.com>
2023-12-14 17:36:26 +0900

    Update page.tsx

--- src/app/change-log/page.tsx
+++ src/app/change-log/page.tsx
@@ -23,8 +23,18 @@ export default async function Home() {
 }
 
 const loadContent = async () => {
+  if (process.env.NODE_ENV === "production") {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/sarayademo/azurechat/main/src/app/change-log/update.md",
+      {
+        cache: "no-cache",
+      }
+    );
+    return await response.text();
+  } else {
     return await fs.readFile(
       process.cwd() + "/app/change-log/update.md",
       "utf8"
     );
+  }
 };
